5b8dc7541f48e6824599f20bdb398d7f77d3b025
osureto1 <117413878+osureto1@users.noreply.github.com>
2023-06-29 14:58:55 -0400

    common.groovy

---
+++ vars/common.groovy
@@ -0,0 +1,16 @@
+def call(string stageName){
+    if ("${stageName}" == "Build")
+    {
+        sh "mvn clean package"
+    }
+
+    else if ("${stageName}" == "CodeAnalysis")
+    {
+        sh "mvn clean sonar:sonar"
+    }
+
+    else if ("${stageName}" == "Upload")
+    {
+        sh "mvn clean deploy"
+    }
+}
